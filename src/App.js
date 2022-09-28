b6366e6434ff127a0374d5871c05d7bbf0c590e0
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2022-09-28 23:33:48 +0800

    initial table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Api from "./services/api";
+import Table from "./component/table";
+import { toHumanTime, getStatus } from "./utils/format";
 
 const App = () => {
+  const [jobs, setJobs] = useState([]);
+
   const onGetJobs = async () => {
-    const jobs = await Api.getJobs();
-    console.log(jobs);
+    const response = await Api.getJobs();
+    if (response) {
+      setJobs(response);
+    }
   };
+
+  const columns = [
+    {
+      Header: "Name",
+      accessor: "client.name",
+    },
+    {
+      Header: "Job",
+      accessor: "title",
+    },
+    {
+      Header: "Status",
+      accessor: "status",
+      Cell: (props) => <p> {getStatus(props.value)}</p>,
+    },
+    {
+      Header: "Created",
+      accessor: "createdAt",
+      Cell: (props) => <p> {toHumanTime(props.value)}</p>,
+    },
+  ];
 
   useEffect(() => {
     onGetJobs();
@@ -14,7 +41,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <p>TEST</p>
+      <h1>Job Dashboard</h1>
+      <Table columns={columns} data={jobs} />
     </div>
   );
 };
